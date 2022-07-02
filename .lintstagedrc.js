8d58e20f2aff985b6f5b403556894705d22c3423
Veyo <veyo@rgpl.xyz>
2022-07-02 14:48:22 +0800

    task: add bootswatch

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -12,7 +12,7 @@ const removeIgnoredFiles = async (files) => {
 }
 
 module.exports = {
-    'src/**/*.{js,jsx,ts,tsx,html,css,scss,sass}': async (files) => {
+    'src/**/*.{js,jsx,ts,tsx,html,css,scss}': async (files) => {
         const filesToLint = await removeIgnoredFiles(files)
         return [`npx prettier --write ${filesToLint}`, `npx eslint ${filesToLint}`]
     },
