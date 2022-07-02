7ede628e60500a63444dfa555cf26639d21ab871
Veyo <veyo@rgpl.xyz>
2022-07-02 19:48:20 +0800

    task: add landing module - use title strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, TitleStrategy } from '@angular/router';
+import { PageTitleStrategy } from './core/title/page-title-strategy';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./features/landing/landing.module').then((m) => m.LandingModule),
+    title: `Home`
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: TitleStrategy, useClass: PageTitleStrategy }]
 })
 export class AppRoutingModule {}
