941d9202fe16a9af00de223d05a1d3fb92ea61e7
Veyo <veyo@rgpl.xyz>
2022-07-02 19:47:31 +0800

    task: use config for nav links

--- src/app/shared/navigation/horizontal-nav/horizontal-nav.component.ts
+++ src/app/shared/navigation/horizontal-nav/horizontal-nav.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationConfig } from 'src/app/core/config/models/config.model';
+import { ConfigService } from 'src/app/core/config/services/config.service';
 
 @Component({
   selector: 'app-horizontal-nav',
   templateUrl: './horizontal-nav.component.html',
   styleUrls: ['./horizontal-nav.component.sass']
 })
-export class HorizontalNavComponent {
-  constructor() {}
+export class HorizontalNavComponent implements OnInit {
+  navigationLinks: NavigationConfig[] | undefined;
+
+  constructor(private configService: ConfigService) {}
+
+  ngOnInit(): void {
+    if (this.configService.configValue) {
+      this.navigationLinks =
+        this.configService.configValue.navigationConfig.filter(
+          (nav) => nav.show
+        );
+    }
+  }
 }
