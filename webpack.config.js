9e04051b4ce872ef11ed6f84563a6077c25ff8dc
Veyo <veyo@rgpl.xyz>
2022-07-04 14:46:17 +0800

    chore: add gzip to the webpack compression

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,13 @@ module.exports = {
   },
   plugins: [
     new CompressionPlugin({
+      filename: '[path][base].gz',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    }),
+    new CompressionPlugin({
       filename: '[path][base].br',
       algorithm: 'brotliCompress',
       test: /\.(js|css|html|svg)$/,
